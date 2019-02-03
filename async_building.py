ea9e90a570ad24bf10977efda17045063695c6ea
roeseth <41995115+roeseth@users.noreply.github.com>
2019-02-03 11:39:00 -0500

    asyncio should be working but stuck in scraping id

--- async_building.py
+++ async_building.py
@@ -9,26 +9,19 @@ import wikipediaapi
 import wptools
 import re
 import json
-from imdbparser import imdbParser
+from async_imdb_parser import imdbParser
 import requests
 
-fetched = set()
-unfetched = set()
 base_url = "http://www.imdb.com/"
 title_url = base_url + "title/"
 search_url = base_url + "find?s=tt&ttype=ft&q="
-json_data = {}
 
 
-def crawl(p):
-    title = p.title
-    entry_name_no_film = re.sub(r' \(film\)| \(2018 film\)', '', title)
-    id_html = scrap_site(search_url + entry_name_no_film)
-    soup = BeautifulSoup(id_html, "lxml")
-    movie = soup.find('td', {'class': 'result_text'}).a
-    id = movie['href'].split('/')[2]
-
-    html = scrap_site(title_url + id + "/")
+async def crawl_id(p, session):
+    print("crawling id: " + p.title)
+    entry_name_no_film = re.sub(r' \(film\)| \(2018 film\)', '', p.title)
+    html = await scrap_site(search_url + entry_name_no_film, session)
+    print('returned id')
     return p, html
 
     # r = await session.get(url)
@@ -37,49 +30,40 @@ def crawl(p):
     # return html
 
 
-def scrap_site(url):
+def parse_id(p, html):
+    print("parsing id: " + p.title)
     try:
-        resp = requests.get(url)
-        html = resp.text()
+        soup = BeautifulSoup(html, 'lxml')
+        movie = soup.find('td', {'class': 'result_text'}).a
+        id = movie['href'].split('/')[2]
+        return p, id
+    except Exception:
+        return p, ''
+
+
+async def crawl_entry(p, id, session):
+    print("crawling entry: " + p.title)
+    html = None
+    if not id == '':
+        html = await scrap_site(title_url + id + "/", session)
+        print('returned id html')
+    return p, html
+
+
+async def scrap_site(url, session):
+    print("scraping url: " + url)
+    try:
+        resp = await session.get(url)
+        html = await resp.text()
+        print("scraping url done: " + url)
         return html
     except Exception:
-        print("Problem with the network connection")
+        print("scraping url ERROR: " + url)
         return None
 
 
-async def main(loop):
-    pool = mp.Pool(2)  # slightly affected
-    async with aiohttp.ClientSession() as session:
-        count = 1
-
-        while len(unfetched) != 0:
-            # tasks = [loop.create_task(crawl(p, session)) for p in unfetched]
-            # done, pending = await asyncio.wait(tasks)
-            # htmls = [f.result() for f in done]
-
-            crawl_jobs = [pool.apply_async(crawl, args = (p,)) for p in unfetched]
-            htmls = [j.get() for j in crawl_jobs]
-
-            parse_jobs = [pool.apply_async(parse_entry, args = (p, html,)) for p, html in htmls]
-            results = [j.get() for j in parse_jobs]
-
-            fetched.update(unfetched)
-            unfetched.clear()
-            for info in results:
-                json_data[count] = info
-                print(info)
-                count += 1
-
-
-# def parse(html):
-#     soup = BeautifulSoup(html, 'lxml')
-#     urls = soup.find_all('a', {"href": re.compile('^/.+?/$')})
-#     title = soup.find('h1').get_text().strip()
-#     page_urls = set([urljoin(base_url, url['href']) for url in urls])
-#     url = soup.find('meta', {'property': "og:url"})['content']
-#     return title, page_urls, url
-
 def parse_entry(p, html):
+    print("parsing entry: " + p.title)
     imdb = imdbParser(p.title, html)
     info = {}
     if imdb.has_match():
@@ -130,15 +114,61 @@ def parse_minutes(str):
     return int(re.match(r'\d+', str).group())
 
 
+async def main(loop, list, json_data):
+    pool = mp.Pool(2)  # slightly affected
+
+    results_id = set()
+    unfetched = list
+    fetched = set()
+    async with aiohttp.ClientSession() as session:
+
+        while len(unfetched) != 0:
+            tasks = [loop.create_task(crawl_id(p, session)) for p in unfetched]
+            done, pending = await asyncio.wait(tasks)
+            htmls = [d.result() for d in done]
+
+            parse_id_jobs = [pool.apply_async(parse_id, args = (p, html,)) for p, html in htmls]
+            results_id = [j.get() for j in parse_id_jobs]
+
+            fetched.update(unfetched)
+            unfetched.clear()
+    print('finished fetching ids')
+    unfetched = results_id
+    fetched.clear()
+    async with aiohttp.ClientSession() as session:
+        count = 1
+        while len(unfetched) != 0:
+
+            tasks = [loop.create_task(crawl_entry(p, id, session)) for p, id in unfetched]
+            done, pending = await asyncio.wait(tasks)
+            htmls = [d.result() for d in done]
+
+            parse_jobs = [pool.apply_async(parse_entry, args = (p, html,)) for p, html in htmls]
+            results = [j.get() for j in parse_jobs]
+
+            fetched.update(unfetched)
+            unfetched.clear()
+            for info in results:
+                json_data[count] = info
+                print(info)
+                count += 1
+
+
 if __name__ == "__main__":
     t1 = time.time()
 
     wiki = wikipediaapi.Wikipedia('en')
     cat = wiki.page("Category:2018 films")
-    unfetched = [wiki.page(p) for p in cat.categorymembers]
+    cat_pages = [wiki.page(p) for p in cat.categorymembers]
+    json_data = {}
+    list = set()
+    for p in cat.categorymembers:
+        page = wiki.page(p)
+        if not page.title.startswith('Category:'):
+            list.add(page)
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(loop))
+    loop.run_until_complete(main(loop, list, json_data))
     loop.close()
 
     # with open('data.json', 'w') as f:
---
+++ async_imdb_parser.py
@@ -0,0 +1,64 @@
+import re
+import requests
+import aiohttp
+from bs4 import BeautifulSoup
+
+
+class imdbParser:
+    baseUrl = "http://www.imdb.com/"
+    titleUrl = baseUrl + "title/"
+    searchUrl = baseUrl + "find?s=tt&ttype=ft&q="
+    title = None
+    soup = None
+    info = {}
+
+    def __init__(self, title, html):
+        self.title = title
+        if not html == None:
+            self.soup = BeautifulSoup(html, 'lxml')
+
+    def has_match(self):
+        return self.soup != None
+
+    def get_director(self):
+        try:
+            self.info['director'] = self.soup.find('div', {'class': 'credit_summary_item'}).a.string
+        except Exception:
+            self.info['director'] = ''
+        return self.info['director']
+
+    def get_runtime(self):
+        try:
+            self.info['runtime'] = re.sub(r'\smin', '', self.soup.find('h4', string = 'Runtime:').parent.time.string)
+        except Exception:
+            self.info['runtime'] = ''
+        return self.info['runtime']
+
+    def get_language(self):
+        try:
+            self.info['language'] = self.soup.find('h4', string = 'Language:').parent.a.string
+        except Exception:
+            self.info['language'] = ''
+        return self.info['language']
+
+    def get_country(self):
+        country = [];
+        try:
+            for tag in self.soup.find('h4', string = 'Country:').parent.find_all('a'):
+                country.append(tag.string)
+            self.info['country'] = country
+        except Exception:
+            self.info['country'] = []
+        return self.info['country']
+
+    def get_cast_list(self):
+        cast_list = []
+        try:
+            for tr in self.soup.find('table', {'class': 'cast_list'}).find_all('tr'):
+                tds = tr.find_all('td')
+                if len(tds) > 2:
+                    cast_list.append(re.sub(r'[\t\r\n]', '', "".join(tds[1].find_all(text = True))).strip())
+            self.info['cast list'] = cast_list
+        except Exception:
+            self.info['cast list'] = []
+        return self.info['cast list']
